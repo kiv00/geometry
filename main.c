512a39c9ac414209f5239f53460aaf34772ebc2e
Корхов Иван Владимирович <ip915s13@awp05.csc.local>
2020-02-17 12:32:59 +0700

    add scan figure

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+char* figure(char* fig){
+ printf("%s \n",fig);
+return 0;
+}
+
+int main()
+{
+char fig[20];
+scanf("%s", fig);
+figure(fig);
+return 0;
+}

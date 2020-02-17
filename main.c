d6d9d70973a1d66d9f75de9aea4f3d289d3c3625
Корхов Иван Владимирович <ip915s13@awp05.csc.local>
2020-02-17 13:03:22 +0700

    create function figure and inicialization circle

--- main.c
+++ main.c
@@ -1,19 +1,34 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 
 
-char* figure(char* fig){
- printf("%s \n",fig);
-return 0;
+int figure(char* fig){
+
+int k=0, swt;
+   for(int i=0; fig[i]!='(';i++){
+k++;
 }
+char check[k];
+for(int i=0; i<k;i++){
+   check[i]=fig[i];
+}
+if(!strcmp(check,"circle")){
+ swt=1;
+}
+return swt;
+}
+
+
 
 int main()
 {
 char fig[20];
 scanf("%s", fig);
-figure(fig);
+int swt=figure(fig);
+printf("%d", swt);
 return 0;
 }

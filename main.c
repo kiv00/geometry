d8e2e1f68887f8b152b5c405d91d38b45335f537
kiv00 <vekmnbrkeynbrr@mail.ru>
2020-02-22 23:04:45 +0700

    add inicializate x coordinate

--- main.c
+++ main.c
@@ -3,32 +3,39 @@
 #include <stdlib.h>
 #include <string.h>
 
+float A[3][10];
 
+void figure(char* fig, int num){
+int k=0;
 
-
-int figure(char* fig){
-
-int k=0, swt;
    for(int i=0; fig[i]!='(';i++){
-k++;
+k=i+1;
 }
 char check[k];
 for(int i=0; i<k;i++){
    check[i]=fig[i];
 }
-if(!strcmp(check,"circle")){
- swt=1;
+if(strcmp(check,"circle@")==0){
+ A[num][1]=1;
 }
-return swt;
+if(A[num][1]==1){
+	A[num][2]=0;
+for(int i=k+1; (float)fig[i]>47 && (float)fig[i]<58;i++){
+	k=i+1;
+A[num][2]=A[num][2]*10+(int)fig[i]-48;
+}
+
+}
 }
 
 
 
 int main()
 {
+	int num=0;
 char fig[20];
-scanf("%s", fig);
-int swt=figure(fig);
-printf("%d", swt);
-return 0;
+	scanf("%s", fig);
+figure(fig,num);
+printf("\n%f ", A[0][2]);
+return 1;
 }

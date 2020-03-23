695259aecf66d1215f6d8c5402df5c95c760db7d
kiv00 <vekmnbrkeynbrr@mail.ru>
2020-03-23 14:31:30 +0700

    add area and perimeter file

---
+++ area.h
@@ -0,0 +1,9 @@
+#ifndef AREA_H
+#define AREA_H
+
+float area(float r){
+	float area=3.14*r*r;
+	return area;
+}
+
+#endif
---
+++ perimeter.h
@@ -0,0 +1,9 @@
+#ifndef PERIMETER_H
+#define PERIMETER_H
+
+float perimeter(float r){
+	float perimeter=3.14*r*2;
+	return perimeter;
+}
+
+#endif
